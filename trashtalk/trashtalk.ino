9665704d2fc7bf228ec23ab0ea3dd172fe495384
David Kirwan <davidkirwanirl@gmail.com>
2016-06-08 01:54:59 +0100

    - WIP

--- trashtalk/trashtalk.ino
+++ trashtalk/trashtalk.ino
@@ -8,24 +8,46 @@
  */
 
 #include <SoftwareSerial.h>
-#include <DFPlayer_Mini_Mp3.h>
+//#include <DFPlayer_Mini_Mp3.h>
 
 int inPin;
+int outPin;
 
 void setup(){
   Serial.begin(9600);
-  mp3_set_serial(Serial);
-  delay(1);
-  mp3_set_volume(15);
-  inPin = A0;
+  //mp3_set_serial(Serial);
+  //delay(1);
+  //mp3_set_volume(15);
+  inPin = 6;
+  outPin = 5;
+  
   pinMode(inPin, INPUT);
+  pinMode(outPin, OUTPUT);
+
+  //mp3_play();
 }
 
 void loop(){
-  int val = analogRead(inPin);
-  if(val == 0){
-    mp3_play();
+  int val = digitalRead(inPin);
+  Serial.println(val);
+  
+  if(val == LOW){
+    delay(1000);
+    val = digitalRead(inPin);
+    Serial.println(val);
+    Serial.println("1st yes");
+    if(val == LOW){    
+      //mp3_play(1);
+      Serial.println("2nd yes");
+        digitalWrite(outPin, LOW);
+      delay(100);
+      digitalWrite(outPin, HIGH);
+    }
+    delay(10000);
   }
-  delay(2000);
+  else {
+    Serial.println("no");
+  }
+  delay(1000);
 }
 
