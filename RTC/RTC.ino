dcad2c29d41577cf82e821a0d6a4fe42bd025712
David Kirwan <davidkirwanirl@gmail.com>
2016-06-28 04:40:15 +0100

    - Real Time Clock module sample code and reference diagram Allow a project to decide whether or not to enable itself based on the time of day. An example would be to switch off projects which generate sound during unsociable hours

---
+++ RTC/RTC.ino
@@ -0,0 +1,68 @@
+/* 
+ *  Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+ *  Get the library: https://github.com/adafruit/RTClib
+ *  Make a decision to enable or disable a project during unsociable hours
+ */
+
+#include <Wire.h>
+#include "RTClib.h"
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+   #define Serial SerialUSB
+#endif
+
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
+
+#ifndef ESP8266
+  while (!Serial); // for Leonardo/Micro/Zero
+#endif
+
+  Serial.begin(57600);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+}
+
+void loop () {
+    DateTime now = rtc.now();
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    int hour = now.hour();
+    if(hour > 21 || hour < 9){
+      Serial.println("Unsociable hours, supress!");
+    }
+    else{
+      Serial.println("all ok, turn it on!");
+    }
+    
+    delay(3000);
+}
