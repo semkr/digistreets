a3c750cc117e010bc2e13992657321ae780cdc5a
David Kirwan <davidkirwanirl@gmail.com>
2016-09-03 01:30:36 +0100

    - minor changes to the codebase, seems to be stable enough now

--- trashtalk/trashtalk_ultrasonic/trashtalk_ultrasonic.ino
+++ trashtalk/trashtalk_ultrasonic/trashtalk_ultrasonic.ino
@@ -14,10 +14,11 @@
 #define TRIGGER_PIN  0  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     1  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-#define PIN_OUT 2
+#define PIN_OUT 2  // Go low to activate MP3 module
+#define PLAY_TIME 15
 
-unsigned int value;
 unsigned int distance;
+unsigned int previous_distance;
 unsigned int tenPer;
 unsigned int low;
 unsigned int high;
@@ -25,53 +26,38 @@ unsigned int high;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void play(){
-  //Serial.println(" Trigger");
   digitalWrite(PIN_OUT, LOW);
   delay(100);
   digitalWrite(PIN_OUT, HIGH);
+  delay(PLAY_TIME * 1000);
 }
 
 void setup() {
   //Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
-  value = 0;
   low = 0;
   high = 0;
+  previous_distance = 0;
 
   pinMode(PIN_OUT, OUTPUT);
   digitalWrite(PIN_OUT, HIGH);
-  delay(2000);      //allow things to stabilize, in case the mp3 module needs time and whatnut
+  delay(3000);      //allow things to stabilize, in case the mp3 module needs time and whatnut
   play(); //trigger the mp3 once on startup just to test everything
   distance = sonar.convert_cm(sonar.ping_median(40));
 }
 
 void loop() {
-  value = sonar.convert_cm(sonar.ping_median(15));
+  distance = sonar.convert_cm(sonar.ping_median(15));
 
-  if (value != 5){
+  if (distance != 5){
     tenPer = (distance/5)+1;
-    if (distance < tenPer) {
-      low = 0;   
-    }
-    else {
-      low = distance - tenPer;  
-    }
+    low = distance - tenPer;
     high = distance + tenPer;
- /* 
-    Serial.print(value); // Send ping, get distance in cm and print result (0 = outside set distance range)
-    Serial.print(", ");
-    Serial.print(tenPer);
-    Serial.print(", ");
-    Serial.print(low);
-    Serial.print(", ");
-    Serial.print(high);
- */ 
-    if(value < low || value > high){
+
+    if(previous_distance < low || previous_distance > high){
       play();
-      delay(3000);
-      distance = sonar.convert_cm(sonar.ping_median(40));
+      previous_distance = distance;
     }
     else {
-      //Serial.println();
     }
   }
 }
